475265367668ce64daa6286fd27be6ca87fbed02
haha58 <11461089222@qq.com>
2023-11-26 15:58:02 +0800

    解决运行兼容性

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint' // 导入包
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     // 增加下面的配置项,这样在运行时就能检查eslint规范
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
